32c6ca81dc2eb7d15602d1c54496b377dae31fab
Kévin Cocchi <kevin.cocchi@gmail.com>
2020-12-02 10:12:18 +0100

    Day 2

---
+++ day02.js
@@ -0,0 +1,36 @@
+const entries = require('./entries/day02.json')
+
+const regex = /^(\d+)-(\d+) ([a-zA-Z]{1}): ([a-zA-Z]*)$/
+
+const formattedEntries = entries.map(s => {
+  const [, min, max, letter, password] = s.match(regex)
+
+  const parsedMin = parseInt(min, 10)
+  const parsedMax = parseInt(max, 10)
+  const foundLetters = password.match(new RegExp(letter, 'g'))
+
+  const firstPositionLetter = password.charAt(parsedMin - 1)
+  const lastPositionLetter = password.charAt(parsedMax - 1)
+
+  return {
+    min: parsedMin,
+    max: parsedMax,
+    letter,
+    letterCount: foundLetters ? foundLetters.length : 0,
+    firstPositionLetter,
+    lastPositionLetter,
+    password,
+  }
+})
+
+// STEP 1
+
+const validEntriesBasic = formattedEntries.filter(obj => obj.min <= obj.letterCount && obj.letterCount <= obj.max)
+
+console.log(validEntriesBasic.length)
+
+// STEP 2
+
+const validEntriesAdvanced = formattedEntries.filter(obj => obj.firstPositionLetter === obj.letter ^ obj.lastPositionLetter === obj.letter)
+
+console.log(validEntriesAdvanced.length)
