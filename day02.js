fb10ac9d06fc544625923dff5281fb66718ee1bd
Kévin Cocchi <kevin.cocchi@gmail.com>
2020-12-04 09:56:14 +0100

    Add Prettier

--- day02.js
+++ day02.js
@@ -1,16 +1,16 @@
-const entries = require('./entries/day02.json')
+const entries = require('./entries/day02.json');
 
-const regex = /^(\d+)-(\d+) ([a-zA-Z]{1}): ([a-zA-Z]*)$/
+const regex = /^(\d+)-(\d+) ([a-zA-Z]{1}): ([a-zA-Z]*)$/;
 
 const formattedEntries = entries.map(s => {
-  const [, min, max, letter, password] = s.match(regex)
+  const [, min, max, letter, password] = s.match(regex);
 
-  const parsedMin = parseInt(min, 10)
-  const parsedMax = parseInt(max, 10)
-  const foundLetters = password.match(new RegExp(letter, 'g'))
+  const parsedMin = parseInt(min, 10);
+  const parsedMax = parseInt(max, 10);
+  const foundLetters = password.match(new RegExp(letter, 'g'));
 
-  const firstPositionLetter = password.charAt(parsedMin - 1)
-  const lastPositionLetter = password.charAt(parsedMax - 1)
+  const firstPositionLetter = password.charAt(parsedMin - 1);
+  const lastPositionLetter = password.charAt(parsedMax - 1);
 
   return {
     min: parsedMin,
@@ -20,17 +20,19 @@ const formattedEntries = entries.map(s => {
     firstPositionLetter,
     lastPositionLetter,
     password,
-  }
-})
+  };
+});
 
 // STEP 1
 
-const validEntriesBasic = formattedEntries.filter(obj => obj.min <= obj.letterCount && obj.letterCount <= obj.max)
+const validEntriesBasic = formattedEntries.filter(obj => obj.min <= obj.letterCount && obj.letterCount <= obj.max);
 
-console.log(validEntriesBasic.length)
+console.log(validEntriesBasic.length);
 
 // STEP 2
 
-const validEntriesAdvanced = formattedEntries.filter(obj => obj.firstPositionLetter === obj.letter ^ obj.lastPositionLetter === obj.letter)
+const validEntriesAdvanced = formattedEntries.filter(
+  obj => (obj.firstPositionLetter === obj.letter) ^ (obj.lastPositionLetter === obj.letter)
+);
 
-console.log(validEntriesAdvanced.length)
+console.log(validEntriesAdvanced.length);
--- day03.js
+++ day03.js
@@ -1,45 +1,45 @@
-const entry = require('./entries/day03.js')
+const entry = require('./entries/day03.js');
 
-const OPEN_SQUARE = '.'
-const TREE = '#'
-const START_POSITION = { x: 1, y: 1 }
+const OPEN_SQUARE = '.';
+const TREE = '#';
+const START_POSITION = { x: 1, y: 1 };
 
-const formattedEntry = entry.default.split('\n')
-const MAX_COL_LENGTH = formattedEntry[0].length
+const formattedEntry = entry.default.split('\n');
+const MAX_COL_LENGTH = formattedEntry[0].length;
 
 const computeSlope = (startPosition, pattern) => {
   const startParams = {
     position: startPosition,
-    path: []
-  }
+    path: [],
+  };
 
   return formattedEntry.reduce((acc, curr, i) => {
-    const { path, position } = acc
+    const { path, position } = acc;
     if (position.y - 1 !== i) return acc;
 
     return {
       path: [...path, curr[(position.x - 1) % MAX_COL_LENGTH]],
       position: {
         x: position.x + pattern.right,
-        y: position.y + pattern.down
-      }
-    }
-  }, startParams).path
-}
+        y: position.y + pattern.down,
+      },
+    };
+  }, startParams).path;
+};
 
-const prodValues = arr => arr.reduce((acc, curr) => acc * curr)
-const countNumberOfTress = path => path.filter(c => c === TREE).length
+const prodValues = arr => arr.reduce((acc, curr) => acc * curr);
+const countNumberOfTrees = path => path.filter(c => c === TREE).length;
 
 // STEP 1
 
 const patternStep1 = {
   right: 3,
-  down: 1
-}
+  down: 1,
+};
 
-const pathStep1 = computeSlope(START_POSITION, patternStep1)
+const pathStep1 = computeSlope(START_POSITION, patternStep1);
 
-console.log(countNumberOfTress(pathStep1))
+console.log(countNumberOfTrees(pathStep1));
 
 // STEP 2
 
@@ -48,9 +48,9 @@ const patternsStep2 = [
   patternStep1,
   { right: 5, down: 1 },
   { right: 7, down: 1 },
-  { right: 1, down: 2 }
-]
+  { right: 1, down: 2 },
+];
 
-const pathsStep2 = patternsStep2.map(pattern => computeSlope(START_POSITION, pattern))
+const pathsStep2 = patternsStep2.map(pattern => computeSlope(START_POSITION, pattern));
 
-console.log(prodValues(pathsStep2.map(countNumberOfTress)))
+console.log(prodValues(pathsStep2.map(countNumberOfTrees)));
